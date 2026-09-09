7357bde9c11a34cc14b668d126fd34b192754843
Mateo Rosa <mateorosa@192.168.1.14>
2026-09-09 19:20:12 -0300

    Add filterByGenre functionality

--- src/domain/GamesList.ts
+++ src/domain/GamesList.ts
@@ -20,10 +20,14 @@ export class GamesList {
   // Si genre es 'All', retornar todos los juegos.
   // Usá el método .filter(x => condición) del array this.games para filtrar por genre.
   filterByGenre(_genre: string): Game[] {
-    return this.games
+    if (_genre === 'All') {
+      return this.games;
+    }
+    return this.games.filter((game) => game.genre === _genre);
   }
 
   count(): number {
     return this.games.length
   }
+  
 }
